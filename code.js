8d34ce8f7662961ceeebb13d90991af0ff1110f3
rencell <rencelltobelonia67@gmail.com>
2024-10-27 22:09:13 +0800

    register.txt

--- code.js
+++ code.js
@@ -1,40 +1,73 @@
-let gonjam = document.querySelectorAll('.movies .list')
-let title = document.getElementById('title')
-let background = document.getElementById('background')
-let texts = document.getElementById('text')
 
 
-gonjam.forEach(heyy => {
-    heyy.addEventListener("click", function(){
-        
-        title.src = "./media/title/" + heyy.id + ".png"
 
-        const image = new Image();
-        imgSrc = "url(./media/background/" +  heyy.id 
-        image.src = imgSrc
-        
-        image.onload = () => {
-    
-            background.style.backgroundImage = imgSrc + ".png)"
-        }
-        image.onerror = () => {
-    
-            background.style.backgroundImage = imgSrc + ".jpg)"
-        }
+fetch('register.txt')
+    .then(response => {
 
-        description(heyy.id).then(result => {
-            texts.innerHTML = result
-        })
+        return response.text()
+    })
+    .then(text => {
+        const arr = text.split(",\r\n");
+
+        arr.forEach(element => {
+            const targetElement = document.querySelector('.movies');
+
+            const list = document.createElement("div");
+            list.className = 'list'
+            list.id = element
+
+            const img = document.createElement("img");
+            img.src = "./media/movies/" + element + ".jpg";
+
+            list.appendChild(img);
+            targetElement.appendChild(list)
+
+            start()
+
+        });
+
+    })
+
+
+function start() {
+    let gonjam = document.querySelectorAll('.movies .list')
+    let title = document.getElementById('title')
+    let background = document.getElementById('background')
+    let texts = document.getElementById('text')
+
+    gonjam.forEach(heyy => {
+        heyy.addEventListener("click", function () {
+
+            title.src = "./media/title/" + heyy.id + ".png"
+
+            const image = new Image();
+            imgSrc = "url(./media/background/" + heyy.id
+            image.src = imgSrc
+
+            image.onload = () => {
+
+                background.style.backgroundImage = imgSrc + ".png)"
+            }
+            image.onerror = () => {
+
+                background.style.backgroundImage = imgSrc + ".jpg)"
+            }
+
+            description(heyy.id).then(result => {
+                texts.innerHTML = result
+            })
+        });
     });
-});
 
-async function description(description) {
-    
-    const response = await fetch('description/' + description +'.txt')
-    const text = await response.text();
-    return text
+    async function description(description) {
+
+        const response = await fetch('description/' + description + '.txt')
+        const text = await response.text();
+        return text
+    }
+
+
 }
-
 
 
 
