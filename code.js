61be5454d01296801d44cb5b1215fed23af77dc7
Jabee-0411 <janice.lumawag2000@gmail.com>
2024-10-28 00:14:24 +0800

    ulit

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ fetch('register.txt')
         return response.text()
     })
     .then(text => {
-        const arr = text.split(",\r\n");
+        const arr = text.split(",/\r?\n/");
 
         arr.forEach(element => {
             const targetElement = document.querySelector('.movies');
