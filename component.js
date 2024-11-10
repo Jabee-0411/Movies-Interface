939ddcc57740f8e7b6e14483b96e91504009f561
Jabee-0411 <janice.lumawag2000@gmail.com>
2024-11-10 15:18:37 +0800

    mobresponsive

--- component.js
+++ component.js
@@ -1,13 +1,39 @@
-window.onscroll = function() {IScroll()}
+window.onscroll = function () { IScroll() }
 
 function IScroll() {
-let scroll = document.getElementById("nav")
+    let scroll = document.getElementById("nav")
 
-    if(document.documentElement.scrollTop > 100){
+    if (document.documentElement.scrollTop > 100) {
         scroll.classList.add("scroll")
-    }else{
+    } else {
         scroll.classList.remove("scroll")
     }
 
 
 }
+let media = window.matchMedia("(max-width: 600px)")
+let options = document.querySelectorAll(".options > ul > li");
+let options2 = document.querySelectorAll(".options > ul > a");
+
+function myMedia(media) {
+    if (media.matches) {
+        for (let i = 0; i < options.length; i++) {
+            options[i].style.display = "none";
+            options2[i].style.display = "block";
+           
+        }
+    }else{
+        for (let i = 0; i < options.length; i++) {
+            options[i].style.display = "block";
+            options2[i].style.display = "none";
+        }
+
+    }
+
+}
+myMedia(media);
+
+media.addEventListener("change", function(){
+    myMedia(media);
+});
+
